6b5a0fb71ffff5e11873de6367f112d92382f847
NeoThermic <michaelkew@hotmail.com>
2014-06-26 22:04:14 +0000

    tweaks

--- rpi/setupAP.sh
+++ rpi/setupAP.sh
@@ -4,5 +4,5 @@ sudo apt-get update && sudo apt-get install hostapd
 echo "Installing DNSmasq"
 sudo apt-get install dnsmasq
 echo "Copy my config file"
-cp -f ~/scripts/rpi/config/dnsmasq.conf ./etc/
+sudo cp -f ~/scripts/rpi/config/dnsmasq.conf /etc/
 echo "Done - reboot then run AP start script"
